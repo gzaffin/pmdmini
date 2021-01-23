b4e8022bcc69e331ce5593af89fdbccae1bf2c3e
gzaffin <gzaffin@gmail.com>
2021-01-23 16:30:22 +0100

    fix #4 malloc.h unavail..

--- src/pmdwin/util.cpp
+++ src/pmdwin/util.cpp
@@ -4,7 +4,6 @@
 //=============================================================================
 
 #include	<stdlib.h>
-#include	<malloc.h>
 #include	<string.h>
 #include	<ctype.h>
 #if defined _WIN32
